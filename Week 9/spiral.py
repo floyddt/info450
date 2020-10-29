b6a4a7bf071c5e0fc37698735a651541010f767d
Damian Floyd <floyddt@vcu.edu>
2020-10-28 22:35:33 -0400

    Attempt

---
+++ Week 9/spiral.py
@@ -0,0 +1,13 @@
+#Steps to solve this problem
+#1. How do I make a 5 by 5 spiral?
+    #a. Input as single number so it can multiply by itself to form the spiral. 
+#2. How do I put the first number in the middle?
+    #a. 2 dimensional array to match coordinates with the number in the array.
+    #b. Make function start from the coordinates of the number 1. 
+#3. How do I increase the count to create the following pattern?
+    #a. Right 1, down 1, left 2 up 2, right 3, down 3, etc.
+#4. How do I sum the diagonal numbers in a spiral?
+    #Use a function to sum numbers pointing to the coordinates along the diagonals
+    #in the 2 dimensional array.
+    
+
